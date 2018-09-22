420755fb3247e9584e5049c0e8ecde291438f099
douglasliralima <douglasliralima@gmail.com>
2018-09-22 11:48:49 -0300

    SO - Project Commented

--- Operating System/escalonador.py
+++ Operating System/escalonador.py
@@ -1,76 +1,106 @@
 import os
-import numpy as np
 from processo import processo
-#Implementar algoritmos de escalonamento(FCFS, SJF, RR)
-#Ler processos com seus respectivos, tempos de chegado e de duração 
-#Saída devera imprimir uma tabela com as métricas de cada processo: Tempo de retorno, resposta e espera média
 
 def leArquivo():
     arq = open('process.txt', 'r')
     texto = arq.readlines()
-    #Vamos fazer um dicionário, onde cada processo terá uma lista com os tempos do processo
-    listaProcess = []
-    listaTempo = []
+    #Vamos fazer um dicionário, com os tempos de entrada e pico de cada processo
     processos = {}
-    maior = 0
     for lines in texto:
         #Vamos ler o tempo de chegada
         dados = lines.split(" ")
         instante = int(dados[0])
         pico = int(dados[1])
+        #Se o processo já existir ele vai para o final da fila daquele processo
         if instante in processos.keys():
             processos[instante].append(pico)
+        #Se o instante nunca foi adicionado ao dicionário, inicializamos a fila
         else:
-            processos[instante] = [pico] #Tranformação implicita em lista desse primeiro instante
+            processos[instante] = [pico] #Tranformação implicita em lista
     arq.close()
     return processos
+
+
 
 def FCFS(processos):
     tResposta = [] #Primeira vez que o processo é startado
     tRetorno = [] #Quando processo finaliza
     tEspera = [] #Enquanto o processo está esperando
 
+    #Primeiro pegamos o tempo total de processamento que vai acontecer
+    iniciais = list(processos.keys())
+
+    #Vamos pegar o maior tempo total possível no pior caso
     tTotal = 0
-    for i in processos.values():
-        tTotal += sum(i)
+    for i in list(processos.keys()):
+        tTotal += i #Ir até o instante do processo
+        soma = 0
+        for j in processos[i]:
+            soma += j
+        tTotal += soma#Ver quanto tempo aquele instante ficará processando
 
-    fila = []
+
+    fila = [] #Será a fila de processos que vai entrar no processador
     cpu = False #Simboliza se a CPU está cheia ou não
-    instanteEntrada = {}
-    nProc = 0 #Vamos já considerar o primeiro processo que vai entrar
-    for tempoAtual in range(tTotal + 10): #+1 pelo instante 0 ser contado
+    nProc = 0 #Auxiliar que representa a quantidade de processos na fila
+
+    #Vamos aqui simular cada segundo em um processador nesse tempo
+    for tempoAtual in range(tTotal):
+
+        #Vamos verificar se estamos em um tempo de entrada de algum processo
         if tempoAtual in processos.keys():
-            #Vamos receber e ordenar os processo que entram
+            #Vamos receber os processo que entram
             fila += processos[tempoAtual]
-            #Vamos somar a quantidade atual de processos que entram para saher quantos ainda tem
+            #Vamos somar a quantidade atual de processos
             nProc += len(processos[tempoAtual])
-            #Guardamos o momento que aquele processo entrou na fila de espera
-            for i in processos[tempoAtual]:
-                instanteEntrada[i] = tempoAtual
 
-
+        #Esse laço vai ver se o processador está livre e colocar um proc caso esteja
         if cpu == False and nProc > 0:
+            #Primeiro vamos pegar o instante de entrada no processador
             instante_inicial = 0
             for instantes in processos.keys():
                 if fila[0] in processos[instantes]:
                     instante_inicial = instantes
+            #E criar o processo
             proc = processo(fila[0], instante_inicial)
-            proc.iniciaProcesso(tempoAtual, instanteEntrada[fila[0]])
+            #Processo criado, ele é startado e retirado da fila
+            proc.iniciaProcesso(tempoAtual, instante_inicial)
             del(fila[0])
-            cpu = True
+            cpu = True #Falamos qua há processo no processador
 
-
+        #Se houver processo no processador o executamos
         elif nProc > 0:
             executado = proc.executaProcesso()
+            #Até que chegamos no momento em que o processo acabe de processar
             if executado == False:
+                #Guardamos os valores de resposta, retorno e espera em nossa "tabela"
                 x, y, z = proc.finalizaProcesso(tempoAtual)
                 tResposta.append(x)
                 tRetorno.append(y)
                 tEspera.append(z)
-                nProc -= 1
-                cpu = False
+                nProc -= 1 #Simboliza que um processo morreu
+                cpu = False #Esvazia a flag da CPU
 
-    return np.mean(tRetorno), np.mean(tResposta), np.mean(tEspera)
+    #Fazemos as médias dos tempos para retornar da função
+    tRetornoMedio = 0.0
+    for i in range(len(tRetorno)):
+        tRetornoMedio += tRetorno[i]
+    tRetornoMedio /= len(tRetorno)
+
+    tRespostaMedio = 0.0
+    for i in range(len(tResposta)):
+        tRespostaMedio += tResposta[i]
+    tRespostaMedio /= len(tResposta)
+
+    tEsperaMedio = 0.0
+    for i in range(len(tEspera)):
+        tEsperaMedio += tEspera[i]
+    tEsperaMedio /= len(tEspera)
+
+    return tRetornoMedio, tRespostaMedio, tEsperaMedio
+
+
+
 
 def SJF(processos):
     tResposta = [] #Primeira vez que o processo é startado
@@ -78,23 +108,29 @@ def SJF(processos):
     tEspera = [] #Enquanto o processo está esperando
 
     
+    #Vamos pegar o maior tempo total possível no pior caso
     tTotal = 0
-    for i in processos.values():
-        tTotal += sum(i)
+    for i in list(processos.keys()):
+        tTotal += i #Ir até o instante do processo
+        soma = 0
+        for j in processos[i]:
+            soma += j
+        tTotal += soma#Ver quanto tempo aquele instante ficará processando
+
 
     fila = []
     cpu = False #Simboliza se a CPU está cheia ou não
     instanteEntrada = {}
     nProc = 0 #Vamos já considerar o primeiro processo que vai entrar
-    for tempoAtual in range(tTotal + 10): #+1 pelo instante 0 ser contado
+
+    for tempoAtual in range(tTotal): #+1 pelo instante 0 ser contado
         if tempoAtual in processos.keys():
             #Vamos receber e ordenar os processo que entram
             fila += processos[tempoAtual]
             #Vamos somar a quantidade atual de processos que entram para saher quantos ainda tem
             nProc += len(processos[tempoAtual])
-            #Guardamos o momento que aquele processo entrou na fila de espera
-            for i in processos[tempoAtual]:
-                instanteEntrada[i] = tempoAtual
+            #A única diferença aqui é essa, quando processos entram na fila, ela
+            #é ordenada
             fila.sort()
 
 
@@ -104,7 +140,7 @@ def SJF(processos):
                 if fila[0] in processos[instantes]:
                     instante_inicial = instantes
             proc = processo(fila[0], instante_inicial)
-            proc.iniciaProcesso(tempoAtual, instanteEntrada[fila[0]])
+            proc.iniciaProcesso(tempoAtual, instante_inicial)
             del(fila[0])
             cpu = True
 
@@ -119,58 +155,89 @@ def SJF(processos):
                 nProc -= 1
                 cpu = False
 
-    return np.mean(tRetorno), np.mean(tResposta), np.mean(tEspera)
+
+    #Fazemos as médias dos tempos para retornar da função
+    tRetornoMedio = 0.0
+    for i in range(len(tRetorno)):
+        tRetornoMedio += tRetorno[i]
+    tRetornoMedio /= len(tRetorno)
+
+    tRespostaMedio = 0.0
+    for i in range(len(tResposta)):
+        tRespostaMedio += tResposta[i]
+    tRespostaMedio /= len(tResposta)
+
+    tEsperaMedio = 0.0
+    for i in range(len(tEspera)):
+        tEsperaMedio += tEspera[i]
+    tEsperaMedio /= len(tEspera)
+
+    return tRetornoMedio, tRespostaMedio, tEsperaMedio
+
+
 
 def RR(processos):
-    tResposta = [] #Primeira vez que o processo é startado
-    tRetorno = [] #Quando processo finaliza
-    tEspera = [] #Enquanto o processo está esperando
+    tResposta = []
+    tRetorno = []
+    tEspera = []
+
 
     
+    #Vamos pegar o maior tempo total possível no pior caso
     tTotal = 0
-    for i in processos.values():
-        tTotal += sum(i)
-    j = 0
+    for i in list(processos.keys()):
+        tTotal += i #Ir até o instante do processo
+        soma = 0
+        for j in processos[i]:
+            soma += j
+        tTotal += soma#Ver quanto tempo aquele instante ficará processando
+
+
     fila = []
     prontos = []
     cpu = False #Simboliza se a CPU está cheia ou não
     instanteEntrada = {}
-    quantum = 1
+
+    #Tempo do quantum, ele é 1, pois representa o instante 0 e 1
+    #Inicializamos ele aqui graças ao segundo if do for abaixo
+    quantum = 1 
+
     nProc = 0 #Vamos já considerar o primeiro processo que vai entrar
-    for tempoAtual in range(tTotal): #+1 pelo instante 0 ser contado
+    for tempoAtual in range(tTotal):
 
         if tempoAtual in processos.keys():
             #Vamos receber e ordenar os processo que entram
             fila += processos[tempoAtual]
-            #Vamos somar a quantidade atual de processos que entram para saher quantos ainda tem
+            #Vamos somar a quantidade atual de processos que entram
             nProc += len(processos[tempoAtual])
-            #Guardamos o momento que aquele processo entrou na fila de espera
-            for i in processos[tempoAtual]:
-                instanteEntrada[i] = tempoAtual
 
 
 
-        #Verificação quanto ao tempo daquele processo
-        if quantum == 0:
-            j += 1
+        #Verificação a partir do 1º processo se o seu quantum acabou
+        if quantum <= 0:
+            #Caso tenha acabado o processo é pausado e reincide na fila
             proc.pausaProcesso(tempoAtual)
             fila.append(proc)
             cpu = False
 
 
         if cpu == False and nProc > 0:
-            quantum = 1 #Segundo inicial já é uma execução e o segundo é 0
-            #Se tiver guardado na fila apenas o tempo de pico, executamos o código abaixo
+            quantum = 1 #Quantum passa a voltar a ser 1, caso tenha sido modificado anteriormente
+            
+            #Se tiver guardado na fila apenas o tempo de pico de um processo
+            #Precisamos inicializar e instanciar o processo da mesma maneira que já
+            #vinhamos fazendo
             if type(fila[0]) == type(int()):
                 instante_inicial = 0
                 for instantes in processos.keys():
                     if fila[0] in processos[instantes]:
                         instante_inicial = instantes
                 proc = processo(fila[0], instante_inicial)
-                proc.iniciaProcesso(tempoAtual, instanteEntrada[fila[0]])
+                proc.iniciaProcesso(tempoAtual, instante_inicial)
                 del(fila[0])
                 cpu = True
-            #Se o próprio processo estiver na fila, executamos o código abaixo
+            #Se o processo já foi criado e está na fila, executamos o código abaixo
+            #Veja que isso só é necessário quando há preempção
             else:
                 proc = fila.pop(0)
                 proc.reiniciaProcesso(tempoAtual)
@@ -179,22 +246,38 @@ def RR(processos):
             
 
         elif nProc > 0:
-            executado = proc.executaProcesso()
-            if executado == False:
+            executado = proc.executaProcesso() #Executamos o processo
+            if executado == False: #Se acabou seu tempo de execução, ele morre
                 x, y, z = proc.finalizaProcesso(tempoAtual)
                 tResposta.append(x)
                 tRetorno.append(y)
                 tEspera.append(z)
                 nProc -= 1
                 cpu = False
+                #E o quantum volta a ser zero
+                #n interessando que instante isso aconteceu
                 quantum = 1
 
-            else:
+            else: #Caso o processo continue executando, diminuimos o quantum
                 quantum -= 1
                         
+    #Fazemos as médias dos tempos para retornar da função
+    tRetornoMedio = 0.0
+    for i in range(len(tRetorno)):
+        tRetornoMedio += tRetorno[i]
+    tRetornoMedio /= len(tRetorno)
 
-    return np.mean(tRetorno), np.mean(tResposta), np.mean(tEspera)
+    tRespostaMedio = 0.0
+    for i in range(len(tResposta)):
+        tRespostaMedio += tResposta[i]
+    tRespostaMedio /= len(tResposta)
 
+    tEsperaMedio = 0.0
+    for i in range(len(tEspera)):
+        tEsperaMedio += tEspera[i]
+    tEsperaMedio /= len(tEspera)
+
+    return tRetornoMedio, tRespostaMedio, tEsperaMedio
 
 
 
--- Operating System/processo.py
+++ Operating System/processo.py
@@ -1,4 +1,5 @@
 class processo:
+    #Inicialização do processo
     def __init__(self, tempo_cpu, instante_inicial):
         self.instante_entrada = 0
         self._tempo_cpu = tempo_cpu
@@ -16,36 +17,41 @@ class processo:
         return self._tempo_cpu
 
 
+    #Caso a gente inicialize o processo pela primeira vez
+    def iniciaProcesso(self, instante_atual, instante_entrada):
+        #Primeiro instante de execução
+        self.tRetorno = 1 #Primeira execução e sua demora para retornar
+        self._tempo_cpu -= 1 #Execução em si
+
+        #Inicialização dos contadores
+        self.tEspera = instante_atual - self.instante_inicial #Caso tenha sido mandado no 4, mas apenas inicializado no 6
+        self.tResposta = instante_atual - self.instante_inicial 
+
+
+    #Caso a gente reinicie o código, ele executa nesse instante de reinicialização
+    #E atualizamos apenas o tempo de espera
     def reiniciaProcesso(self, instante_atual):
         #Reinizaliza execução
         self._tempo_cpu -= 1
         self.tEspera += (instante_atual - self.instante_entrada)
 
 
-    def iniciaProcesso(self, instante_atual, instante_entrada):
-        #Primeiro instante de execução
-        self.tRetorno = 1
-        self._tempo_cpu -= 1
-
-        #Inicialização dos contadores
-        self.tEspera = instante_atual - self.instante_inicial
-        self.tResposta = instante_atual - instante_entrada
-
-
+    #Caso a gente queira apenas executar o processo
     def executaProcesso(self):
-        #Executa
-        self._tempo_cpu -= 1
-        if self._tempo_cpu <= 0:
+        self._tempo_cpu -= 1 #Executa
+        if self._tempo_cpu <= 0: #Caso o seu tempo tenha acabado
             return False #Processo finalizado e não pode executar mais
-        elif self._tempo_cpu > 0:
+        elif self._tempo_cpu > 0: #Caso não
             return True #Processo finalizado e executou 1 segundo
 
 
     def pausaProcesso(self, instante_atual):
-        #Quando o processo é pausado, ele volta a fila, o que se torna atualmente o seu instante entrada 
+        #Quando o processo é pausado, ele volta a fila, o que se torna atualmente o seu instante entrada na fila
         self.instante_entrada = instante_atual
         
-
+    #Verificamos quanto tempo demorou para executar
+    #Retornamos todos os tempos daquele processo
     def finalizaProcesso(self, instante_atual):
-        self.tRetorno = (instante_atual - self.instante_inicial) + 1#Somo com 1 para contabilizar o começo do próximo segundo
+        #Somo com 1 para contabilizar o começo do segundo de finalização
+        self.tRetorno = (instante_atual - self.instante_inicial) + 1
         return self.tResposta, self.tRetorno, self.tEspera
